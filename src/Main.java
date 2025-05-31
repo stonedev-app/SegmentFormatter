bca0e58b9b2815070e7450ed701c39f73ac3e866
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2025-05-31 22:50:27 +0900

    フォーマッターの使い方にコメント追加

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
         // フォーマット対象文字列
-        String str = "1234568";
-        System.out.println(str);
+        String str = "1234567";
+        // フォーマット前文字列を出力
+        System.out.printf("フォーマット前文字列:%s%n", str);
 
         // 区切り文字フォーマット処理を生成
+        // 区切り文字:-
+        // 何文字毎に区切り文字追加:2
+        // 右から区切り文字追加:true
         var formatter = SegmentFormat.createFormatter(
                 "-",
                 2,
                 true);
 
-        // ハイフンを右から2文字毎に追加
+        // フォーマット処理実行
         var resultStr = formatter.apply(str);
 
-        // ハイフン追加後文字列
-        System.out.println(resultStr);
+        // フォーマット後文字列を出力
+        // 出力結果:1-23-45-67
+        System.out.printf("フォーマット後文字列:%s%n", resultStr);
     }
 }
